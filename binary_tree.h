268dd54d9994766a36660983d85e9a5518028024
LucasMota10 <lucasbulcaobmota@gmail.com>
2025-01-04 22:10:14 -0300

    tree_att

--- binary_tree.h
+++ binary_tree.h
@@ -4,23 +4,22 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-typedef struct node{
+typedef struct node {
     int val;
     struct node *smaller;
     struct node *bigger;
-}node;
+} node;
 
-typedef struct binary_tree{
-    node* root;
+typedef struct binary_tree {
+    node *root;
     int quantity;
-}binary_tree;
+} binary_tree;
 
-node* create_node(int x){
-
+node* create_node(int x) {
     node *aux = (node *)malloc(sizeof(node)); 
 
     if (aux == NULL) {
-        printf("Error: failure to allocate memory.\n");
+        printf("Erro: Falha ao alocar memória.\n");
         return NULL;
     }
     aux->bigger = NULL;
@@ -29,126 +28,121 @@ node* create_node(int x){
     return aux;
 }
 
-binary_tree* inicialize_tree(binary_tree *tree){
+binary_tree* inicialize_tree(binary_tree *tree) {
     tree->quantity = 0;
     tree->root = NULL;
     return tree;
 }
 
-bool empty(binary_tree *tree){
-
-    if(tree->quantity == 0){
-        return true;
-    }else{
-        return false;
-    }
-    
+bool empty(binary_tree *tree) {
+    return tree->quantity == 0;
 }
 
-void insert_node(binary_tree *tree, node *x){
-    if(!empty(tree)){
+void insert_node(binary_tree *tree, node *x) {
+    if (empty(tree)) { // Configura a raiz se a árvore estiver vazia
         tree->root = x;
         tree->quantity++;
-    }else{
-        node *aux = tree->root;
-        while(1){
-            if(x->val < aux->val){
-                if(aux->smaller == NULL){
-                    aux->smaller = x;
-                    tree->quantity++;
-                    break;
-                }else{
-                    aux = aux->smaller;
-                    continue;
-                }
-            }
-            else if(x->val > aux->val){
-                if(aux->bigger == NULL){
-                    aux->bigger = x;
-                    tree->quantity++;
-                    break;
-                }else{
-                    aux = aux->bigger;
-                    continue;
-                }
-            }
-            else{
-                break;
-            }
-        }
-    }
-}
-
-bool remove_node(int x, binary_tree *tree) {
-    if (empty(tree)) {
-        printf("Error: The tree is empty\n");
-        return false;
+        return;
     }
 
     node *aux = tree->root;
-    node *aux2 = NULL;
-
-    while (aux != NULL) {
-        if (x > aux->val) {
-            aux2 = aux;
-            aux = aux->bigger;
-        } else if (x < aux->val) {
-            aux2 = aux;
-            aux = aux->smaller;
-        } else { // Nó encontrado
-            // Caso 1: Nó sem filhos
-            if (aux->bigger == NULL && aux->smaller == NULL) {
-                if (aux2 == NULL) { // É a raiz
-                    tree->root = NULL;
-                } else if (aux2->bigger == aux) {
-                    aux2->bigger = NULL;
-                } else {
-                    aux2->smaller = NULL;
-                }
-                free(aux);
+    while (1) {
+        if (x->val < aux->val) {
+            if (aux->smaller == NULL) {
+                aux->smaller = x;
+                tree->quantity++;
+                break;
+            } else {
+                aux = aux->smaller;
             }
-            // Caso 2: Nó com um único filho
-            else if (aux->bigger == NULL || aux->smaller == NULL) {
-                node *child = (aux->smaller != NULL) ? aux->smaller : aux->bigger;
-                if (aux2 == NULL) { // É a raiz
-                    tree->root = child;
-                } else if (aux2->bigger == aux) {
-                    aux2->bigger = child;
-                } else {
-                    aux2->smaller = child;
-                }
-                free(aux);
+        } else if (x->val > aux->val) {
+            if (aux->bigger == NULL) {
+                aux->bigger = x;
+                tree->quantity++;
+                break;
+            } else {
+                aux = aux->bigger;
             }
-            // Caso 3: Nó com dois filhos
-            else {
-                node *successor = aux->bigger;
-                node *successor_parent = aux;
-
-                while (successor->smaller != NULL) {
-                    successor_parent = successor;
-                    successor = successor->smaller;
-                }
-
-                // Substituir valor
-                aux->val = successor->val;
-
-                // Ajustar ponteiros do sucessor
-                if (successor_parent->smaller == successor) {
-                    successor_parent->smaller = successor->bigger;
-                } else {
-                    successor_parent->bigger = successor->bigger;
-                }
-                free(successor);
-            }
-
-            tree->quantity--; // Decrementar após remoção
-            return true;
+        } else {
+            // Valor já existe na árvore, não insere novamente
+            break;
         }
     }
+}
 
-    // Se não encontrar o valor
-    printf("Error: Node with value %d not found\n", x);
-    return false;
+void print_infixo(node *aux) {
+    if (aux == NULL) {
+        return;
+    }
+
+    print_infixo(aux->smaller); // Subárvore esquerda
+    printf("%d\n", aux->val);  // Nó atual
+    print_infixo(aux->bigger); // Subárvore direita
+}
+
+void print_prefix(node *aux){
+    if(aux == NULL){
+        return ;
+    }
+
+    printf("%d\n",aux->val);
+    print_prefix(aux->smaller);
+    print_prefix(aux->bigger);
+}
+
+void print_posfix(node *aux){
+    if(aux == NULL){
+        return;
+    }
+
+    print_posfix(aux->smaller);
+    print_posfix(aux->bigger);
+    printf("%d\n",aux->val);
+}
+
+bool val_exist(node *aux,int x){
+
+    if(aux == NULL){
+        return false;
+    }
+
+    if(aux->val == x){
+        return true;
+    }
+
+    if(x > aux->val){
+        val_exist(aux->bigger,x);
+    }else{
+        val_exist(aux->smaller,x);
+    }
+}
+
+int max_val(binary_tree tree){
+
+    if(empty(&tree)){
+        return -1;
+    }
+
+    node* aux = tree.root;
+
+    while(aux->bigger != NULL){
+        aux = aux->bigger;
+    }
+    return aux->val;
+}
+
+int min_val(binary_tree tree){
+
+    if(empty(&tree)){
+        return -1;
+    }
+    
+    node* aux = tree.root;
+
+    while(aux->smaller != NULL){
+        aux = aux->smaller;
+    }
+    return aux->val;
 }
 
 #endif
